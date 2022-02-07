1dd514a76173cc5e30cbf17069559d46d12cc696
Clark Brace <cbrace@pugetsound.edu>
2022-02-07 02:13:29 -0500

    added comments and finishing touches

--- main.c
+++ main.c
@@ -11,33 +11,40 @@ int main()
 	char playerinput; //checks if the player wants to contenue
 	
 
-	while (playerinput != 'n' || numElements > 128)
+	while (playerinput != 'n' || numElements > 128) //the main loop for adding names
 	{
 		char name[MAX_NAME_LEN];
 		int salary;
 		
+		//get name and salary
 		printf("Name: ");
 		scanf("%s",name);
 		printf("Salary: ");
 		scanf("%d",&salary);
 		printf("\n");
 
+		//add infor to a temp employee
 		struct Employee temp;
 		strcpy(temp.name, name);
 		temp.salary = salary;
 		
+		//adds elements to array
 		A[numElements] = temp;
 		numElements++;
 
+		//get next input
 		printf("Enter another user (y/n)? ");
 		scanf(" %c" , &playerinput);
 
 
+		//make sure the input for if you want to enter another is good.
 		int flag = 0;
 		while(flag == 0){
+			//check if input is y or n
 			if(playerinput == 'n' || playerinput == 'y'){
 				break;
 			}
+				//repeat the input if you enter something incorrect
 			printf("\nYour prevous input was: %c", playerinput);
 			printf("\nSorry, could not parse your responce. Please enter either y or n: ");
 		 	scanf(" %c" , &playerinput);
@@ -47,58 +54,11 @@ int main()
 		
 	}
 	
-	
-	
-	
-	
-	
-	// struct Employee a0 = {"a0", 55};
-	// struct Employee a1 = {"a1", 42};
-	// struct Employee a2 = {"a2", 99};
-	// struct Employee a3 = {"a3", 90};
-	// struct Employee a4 = {"a4", 73};
-	// struct Employee a5 = {"a5", 81};
-	// struct Employee a6 = {"a6", 84};
-	// struct Employee a7 = {"a7", 93};
-	// struct Employee a8 = {"a8", 79};
-	// struct Employee a9 = {"a9", 97};
-	// struct Employee a10 = {"a10", 83};
-	// struct Employee a11 = {"a11", 21};
 
-	// A[0] = a0;
-	// A[1] = a1;
-	// A[2] = a2;
-	// A[3] = a3;
-	// A[4] = a4;
-	// A[5] = a5;
-	// A[6] = a6;
-	// A[7] = a7;
-	// A[8] = a8;
-	// A[9] = a9;
-	// A[10] = a10;
-	// A[11] = a11;
-
-	
-
-	printList(A,numElements);
-
-	//heapify(A,2,numElements);
-	buildHeap(A,numElements);
-	printf("\n");
-
-	printList(A,numElements);
-
-	// printList(A,4);
-	// swap(&A[0], &A[2]);
-	// printList(A,4);
+	buildHeap(A,numElements); //builds and sorts the heap
 
 
-	//heapify()
-
-
-
-	
-
+	printList(A,numElements); //prints the results
 
 	
 	return 0;
